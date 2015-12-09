e5ad28cb95c171f3081510a1e18237b5f4caf433
MUSTLab Developer <hello@mustlab.ru>
2015-12-09 15:08:43 +0300

    added unique checks and tests

---
+++ test/schedules.js
@@ -0,0 +1,129 @@
+"use strict";
+
+var expect = require('chai').expect;
+var moment = require('moment');
+var KueScheduler = require('../');
+
+describe("schedule", function() {
+    this.timeout(60000);
+
+    var queue = KueScheduler.createQueue();
+
+    it('schedule now', function (done) {
+        let job_name = 'test_now_' + Date.now();
+
+        var job = queue.create(job_name, { some_field: 'with value' });
+
+        queue.now(job, function (result) {
+            if (result instanceof Error) {
+                done(result);
+            }
+        });
+
+        queue.process(job_name, function (job, ctx, done_job) {
+            console.log("running",job_name);
+            done_job();
+            done();
+            job.remove();
+        });
+    });
+
+    it('schedule with human interval', function (done) {
+        let job_name = 'test_once_human_' + Date.now();
+
+        var job = queue.create(job_name, { some_field: 'with value' });
+
+        queue.schedule('1 second',job, function (result) {
+            if (result instanceof Error) {
+                done(result);
+            }
+        });
+
+        queue.process(job_name, function (job, ctx, done_job) {
+            console.log("running",job_name);
+            done_job();
+            done();
+            job.remove();
+        });
+    });
+
+    it('schedule with date', function (done) {
+        let job_name = 'test_once_date_' + Date.now();
+
+        var job = queue.create(job_name, { some_field: 'with value' });
+
+        let scheduledDate = moment().add(1,'seconds').toDate();
+
+        queue.schedule(scheduledDate,job, function (result) {
+            if (result instanceof Error) {
+                done(result);
+            }
+        });
+
+        queue.process(job_name, function (job, ctx, done_job) {
+            //console.log("running",job_name);
+            done_job();
+            done();
+            job.remove();
+        });
+    });
+
+    it('schedule with recurrence', function (done) {
+        let job_name = 'test_recurrence_' + Date.now();
+
+        var job = queue.create(job_name, { some_field: 'with value' });
+
+        queue.every('1 second',job, function (result) {
+            if (result instanceof Error) {
+                done(result);
+            }
+        });
+
+        var counter = 0;
+
+        queue.process(job_name, function (job, ctx, done_job) {
+            //console.log("running",job_name);
+            done_job();
+
+            counter++;
+
+            if (counter == 2) {
+                done();
+            }
+
+            setTimeout(job.remove,2000);
+        });
+    });
+
+    it('schedule with recurrence handling failure', function (done) {
+        let job_name = 'test_recurrence_failure_' + Date.now();
+
+        var job = queue.create(job_name, { some_field: 'with value' });
+
+        queue.every('1 second',job, function (result) {
+            if (result instanceof Error) {
+                done(result);
+            }
+        });
+
+        var counter = 0;
+
+        queue.process(job_name, function (job, ctx, done_job) {
+            //console.log("running",job_name);
+
+            if (counter == 0) {
+                done_job(new Error('simulated error'));
+            } else {
+                done_job();
+
+                if (counter == 1) {
+                    done();
+                }
+            }
+
+            counter++;
+
+            setTimeout(job.remove,2000);
+        });
+    });
+});
---
+++ test/unique.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var expect = require('chai').expect;
+var moment = require('moment');
+var KueScheduler = require('../');
+
+describe("unique", function() {
+    this.timeout(60000);
+
+    var queue = KueScheduler.createQueue();
+
+    it('unique double schedule', function (done) {
+        let unique = ''+Date.now();
+
+        let job_name = 'test_unique_double_' + unique;
+
+        var job = queue.create(job_name, { some_field: 'with value 1' }).unique(unique);
+
+        queue.schedule('1 second',job, function (result) {
+            if (result instanceof Error) {
+                done(result);
+            }
+
+            var job2 = queue.create(job_name, { some_field: 'with value 2' }).unique(unique);
+
+            queue.schedule('2 second',job2, function (result) {
+                if (result instanceof Error) {
+                    done(result);
+                }
+            });
+        });
+
+        queue.process(job_name, function (job, ctx, done_job) {
+            console.log("running:",job_name,'unique:',job.uniqueKey());
+            done_job();
+
+            expect(job.data.some_field).to.equal('with value 2');
+
+            done();
+        });
+    });
+
+    it('unique remove', function (done) {
+        let unique = ''+Date.now();
+
+        let job_name = 'test_unique_remove_' + unique;
+
+        var job = queue.create(job_name, { some_field: 'with value 1' }).unique(unique);
+
+        queue.schedule('2 second',job, function (result) {
+            if (result instanceof Error) {
+                done(result);
+            }
+
+            queue.removeUniqueJob(unique, function (err) {
+                if (err) return done(err);
+
+                setTimeout(done,3000);
+            });
+        });
+
+        queue.process(job_name, function (job, ctx, done_job) {
+            console.log("running:",job_name,'unique:',job.uniqueKey());
+            done_job();
+            done(new Error('job shouldn\'t have been started as it is removed'));
+        });
+    });
+});
